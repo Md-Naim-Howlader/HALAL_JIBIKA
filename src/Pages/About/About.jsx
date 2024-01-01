2c83d5271380158450f6b70c5af18b9ddc76ae3d
Code With Naim <111947358+CodeWithNaim@users.noreply.github.com>
2024-01-01 09:54:53 +0600

    [DONE] About Page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,47 @@
+import "./about.css";
 const About = () => {
   return (
-    <section>
-      <div className="container">
-        <h1>About Page</h1>
+    <section className="about__section">
+      <div>
+        <div className="container">
+          <div style={{ paddingTop: "50px" }} className="section__title">
+            <span>ABOUT US</span>
+            <h2>ABOUT US</h2>
+          </div>
+          <div className="mission">
+            <p>Our mission:</p>
+            <h1>
+              WE ACTIVELY <br /> CONNECT PEOPLE TO THEIR NEXT GREAT OPPORTUNITY.
+            </h1>
+          </div>
+        </div>
+        <div className="achivements__area">
+          <div className="container">
+            <p>
+              What started as a way to help small businesses find great
+              candidates has grown into a leading online employment marketplace
+              that connects millions of job seekers with companies of all sizes.
+            </p>
+            <div className="achivements ">
+              <div className="achivement">
+                <h2>#1</h2>
+                <p>rated job search app on iOS and Android</p>
+              </div>
+              <div className="achivement">
+                <h2>162M+</h2>
+                <p>job seekers have used HALAL JIBIKA</p>
+              </div>
+              <div className="achivement">
+                <h2>3.8M+</h2>
+                <p>businesses have used HALAL JIBIKA</p>
+              </div>
+              <div className="achivement">
+                <h2>#1</h2>
+                <p>rated hiring site in the U.S.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
