22ab29c0f307824d8e6726538506f67a238d92a1
Md-Naim-Howlader <mdnaimptk776@gmail.com>
2024-01-29 15:18:14 +0600

    change server URL

--- src/baseURL/baseURL.js
+++ src/baseURL/baseURL.js
@@ -1,7 +1,1 @@
-export const baseURL = "http://localhost:9000/jobs";
-
-// local json server link
-// http://localhost:9000/jobs
-
-// online live json server link
-// https://json-server-6cgn.onrender.com/jobs
+export const baseURL = "https://json-server-6cgn.onrender.com/jobs";
