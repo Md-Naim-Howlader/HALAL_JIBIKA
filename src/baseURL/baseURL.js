aa70ec514fcf4fc0acf898ff592cfd89f876603a
Md-Naim-Howlader <mdnaimptk776@gmail.com>
2024-01-31 13:14:20 +0600

    url uncomment

--- src/baseURL/baseURL.js
+++ src/baseURL/baseURL.js
@@ -1,2 +1,1 @@
-// export const baseURL = "https://json-server-6cgn.onrender.com/jobs";
-export const baseURL = "http://localhost:9000/jobs";
+export const baseURL = "https://json-server-6cgn.onrender.com/jobs";
